2a11b89541a22f744f8b8aea75508e29f717ddc5
Sedale McCall <Sedale McCall>
2016-08-28 18:10:34 -0400

    module 4 solution

---
+++ solution4/SpeakGoodBye.js
@@ -0,0 +1,10 @@
+(function(window, name){
+var byeSpeaker = {};
+var speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker;
+}) (window, name);
